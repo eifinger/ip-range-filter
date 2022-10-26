1b8f8028711a264739f19c90791147c98ae1e5e3
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2022-10-26 19:34:53 +0200

    feat: Initializr Template

---
+++ src/main/java/de/kevinstillhammer/iprangefilter/IpRangeFilterApplication.java
@@ -0,0 +1,13 @@
+package de.kevinstillhammer.iprangefilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpRangeFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpRangeFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kevinstillhammer/iprangefilter/IpRangeFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kevinstillhammer.iprangefilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpRangeFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
