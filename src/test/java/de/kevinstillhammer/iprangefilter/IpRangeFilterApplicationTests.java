7deb15a34db5ea93de21dccef760324bca31548d
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2022-10-27 13:20:02 +0200

    test: Check for correct MIME Type

--- src/test/java/de/kevinstillhammer/iprangefilter/IpRangeFilterApplicationTests.java
+++ src/test/java/de/kevinstillhammer/iprangefilter/IpRangeFilterApplicationTests.java
@@ -31,62 +31,63 @@ import static org.mockserver.model.HttpResponse.response;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(initializers = {IpRangeFilterApplicationTests.MockServerInitializer.class}, classes = {IpRangeFilterApplication.class})
+@ContextConfiguration(initializers = {IpRangeFilterApplicationTests.MockServerInitializer.class},
+                      classes = {IpRangeFilterApplication.class})
 @Testcontainers
-class IpRangeFilterApplicationTests{
+class IpRangeFilterApplicationTests {
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	MockServerClient mockServerClient;
+    MockServerClient mockServerClient;
 
-	@Container
-	public static final MockServerContainer mockServer = new MockServerContainer(
-			DockerImageName
-					.parse(
-							"mockserver/mockserver"
-					).withTag("mockserver-" + MockServerClient.class.getPackage().getImplementationVersion())
-	);
+    @Container
+    public static final MockServerContainer mockServer = new MockServerContainer(DockerImageName
+            .parse("mockserver/mockserver")
+            .withTag("mockserver-" + MockServerClient.class
+                    .getPackage()
+                    .getImplementationVersion()));
 
-	@Value("classpath:ip_ranges_26_10_2022.json")
-	Resource ipRangeFile;
+    @Value("classpath:ip_ranges_26_10_2022.json")
+    Resource ipRangeFile;
 
-	@BeforeAll
-	public void setUp() throws IOException {
-		mockServerClient = new MockServerClient(mockServer.getHost(), mockServer.getServerPort());
-			mockServerClient.when(request()
-							.withPath("/ip-ranges.json"))
-					.respond(response()
-							.withBody(StreamUtils.copyToString(ipRangeFile.getInputStream(), Charset.defaultCharset()))
-							.withContentType(MediaType.APPLICATION_JSON));
-	}
+    @BeforeAll
+    public void setUp() throws IOException {
+        mockServerClient = new MockServerClient(mockServer.getHost(), mockServer.getServerPort());
+        mockServerClient
+                .when(request().withPath("/ip-ranges.json"))
+                .respond(response()
+                        .withBody(StreamUtils.copyToString(ipRangeFile.getInputStream(), Charset.defaultCharset()))
+                        .withContentType(MediaType.APPLICATION_JSON));
+    }
 
-	@AfterAll
-	public void tearDown() {
-		mockServerClient.close();
-	}
+    @AfterAll
+    public void tearDown() {
+        mockServerClient.close();
+    }
 
-	@Test
-	void defaultShouldContainKnownIPRange() {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/ip-ranges",
-				String.class)).contains("3.2.34.0/26");
-	}
+    @Test
+    void defaultShouldContainKnownIPRange() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/ip-ranges", String.class)).contains("3.2.34.0/26");
+    }
 
-	public static class MockServerInitializer
-			implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Test
+    void resultShouldBeTextPlain() {
+        assertThat(this.restTemplate
+                .getForEntity("http://localhost:" + port + "/ip-ranges", String.class)
+                .getHeaders()
+                .getContentType()).hasToString("text/plain;charset=UTF-8");
+    }
 
-
-
-		@Override
-		public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
-			mockServer.start();
-			TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
-					configurableApplicationContext, "awsUrl=" + mockServer.getEndpoint() + "/ip-ranges.json");
-		}
-
-
-	}
+    public static class MockServerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
+            mockServer.start();
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(configurableApplicationContext,
+                    "awsUrl=" + mockServer.getEndpoint() + "/ip-ranges.json");
+        }
+    }
 }
